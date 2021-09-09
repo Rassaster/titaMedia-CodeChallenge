bc67c986eaa7d08e491436e20707bab6daa9a0d3
Juan Manuel Rassa <jmrassaproduccion@gmail.com>
2021-09-09 11:54:28 -0500

    Adding comments, refactoring and organizing code.

--- src/model/fetchUnsplash.js
+++ src/model/fetchUnsplash.js
@@ -1,21 +1,19 @@
-const lg = console.log;
+//  Constants for fetching Unsplash API:
 const unsplash_BaseURL = "https://api.unsplash.com";
 const unsplash_APIKey = "BumccbjwDAHEq1iVsADbyN1LR6pFJWtRUXxiPEVHTAg";
 const unsplash_SearchEndpoint = '/search/photos?';
 const unsplash_Query = 'query=';
 const unsplash_Page = '&page=';
 const unsplash_PerPage = '&per_page=';
-
+//  Preparing request Headers and Options:
 let unsplashHeaders = new Headers();
 unsplashHeaders.append("Authorization", `Client-ID ${unsplash_APIKey}`);
 unsplashHeaders.append("Accept-Version", "v1");
-
 let unsplashRequestOptions = {
   method: 'GET',
   headers: unsplashHeaders,
-  redirect: 'follow'
 };
-
+//  Fetch GET to Unsplash API. Returns an array of objects of each image:
 export const unsplash_triggerSearch = async (query, page, perPage) => {
   let response = await fetch(`${unsplash_BaseURL}${unsplash_SearchEndpoint}${unsplash_Query}${query}${unsplash_Page}${page}${unsplash_PerPage}${perPage}`, unsplashRequestOptions);
   let data = await response.json();
